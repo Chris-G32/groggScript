8c8b02a07c4b1084032309b3096c1498a43ed5f4
Christopher Gruska <98664133+Chris-G32@users.noreply.github.com>
2026-02-22 16:50:31 -0500

    Delete joke.py

--- joke.py
+++
@@ -1,5 +0,0 @@
-resp=input("Can i tell you a joke, its a diry one? (y/N)")
-if(resp.lower()=="n" or resp.lower()=="no"):
-    print("FUCK YOU MOTHERFUCKER SUCK MY BIG JIGGLY NUTS, THEY ARE STINKY AND WRETCHED")
-else:
-    print("You walk into a bar and you go to the ordering table, and then ... not the bar a place, a milkshake place, a place who sells milkshakes, aaaannnd you ask the worker and you say, 'Hey, can I get a shake perchance?' Whats next? and then and then you say, how about you jiggle my n-, my n - you jiggle my nuts in your face, wait no. Not jiggle. Shake my jiggly nuts in my face. THE END 8--3")
